1946f6ec57cd4439e1230fc3b567fe836dabe04c
Tan <p.tanvu@gmail.com>
2025-09-08 14:07:48 +0200

    switching format sql server version table schemas

--- table_schemas_sql_server_version.py
+++ table_schemas_sql_server_version.py
@@ -1,4 +1,5 @@
--- Customers
+# Customers
+customers_table_sql = """
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
     customer_id UNIQUEIDENTIFIER,
@@ -11,8 +12,9 @@ CREATE TABLE customers (
     CONSTRAINT customer_zip_code_prefix_valid CHECK (customer_zip_code_prefix LIKE '[0-9][0-9][0-9][0-9]%'),
     CONSTRAINT customer_state_format CHECK (customer_state LIKE '[A-Z][A-Z]')
 );
-
--- Geolocation
+"""
+#Geolocation
+geolocation_table_sql = """
 DROP TABLE IF EXISTS geolocation;
 CREATE TABLE geolocation (
     geolocation_zip_code_prefix VARCHAR(5),
@@ -24,8 +26,9 @@ CREATE TABLE geolocation (
     CONSTRAINT geolocation_zip_code_prefix_valid CHECK (geolocation_zip_code_prefix LIKE '[0-9][0-9][0-9][0-9]%'),
     CONSTRAINT geolocation_state_format CHECK (geolocation_state LIKE '[A-Z][A-Z]')
 );
-
--- Orders
+"""
+#Orders
+orders_table_sql = """
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
     order_id UNIQUEIDENTIFIER,
@@ -40,8 +43,9 @@ CREATE TABLE orders (
     CONSTRAINT PK__orders PRIMARY KEY (order_id),
     CONSTRAINT FK__orders FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
-
--- Order Items
+"""
+#OrderItems
+orderItems_table_sql ="""
 DROP TABLE IF EXISTS order_items;
 CREATE TABLE order_items (
     order_id UNIQUEIDENTIFIER,
@@ -57,8 +61,9 @@ CREATE TABLE order_items (
     CONSTRAINT FK__order_items_product FOREIGN KEY (product_id) REFERENCES products(product_id),
     CONSTRAINT FK__order_items_seller FOREIGN KEY (seller_id) REFERENCES sellers(seller_id)
 );
-
--- Order Payments
+    """
+#OrderPayments
+orderPayments_table_sql = """
 DROP TABLE IF EXISTS order_payments;
 CREATE TABLE order_payments (
     order_id UNIQUEIDENTIFIER NOT NULL,
@@ -70,8 +75,9 @@ CREATE TABLE order_payments (
     CONSTRAINT PK__order_payments PRIMARY KEY (order_id, payment_sequential),
     CONSTRAINT FK__order_payments FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
-
--- Order Reviews
+    """
+#OrderReviews
+orderReviews_table_sql = """
 DROP TABLE IF EXISTS order_reviews;
 CREATE TABLE order_reviews (
     review_id UNIQUEIDENTIFIER,
@@ -86,8 +92,9 @@ CREATE TABLE order_reviews (
     CONSTRAINT FK__order_reviews FOREIGN KEY (order_id) REFERENCES orders(order_id),
     CONSTRAINT review_score CHECK (review_score BETWEEN 1 AND 5)
 );
-
--- Products
+    """
+#Products
+products_table_sql = """
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
     product_id UNIQUEIDENTIFIER,
@@ -103,8 +110,9 @@ CREATE TABLE products (
     CONSTRAINT PK__products PRIMARY KEY (product_id),
     CONSTRAINT FK__products FOREIGN KEY (product_category_name) REFERENCES product_category_name_translation(product_category_name)
 );
-
--- Sellers
+    """
+#Sellers
+sellers_table_sql = """
 DROP TABLE IF EXISTS sellers;
 CREATE TABLE sellers (
     seller_id UNIQUEIDENTIFIER,
@@ -116,8 +124,9 @@ CREATE TABLE sellers (
     CONSTRAINT seller_zip_code_prefix_valid CHECK (seller_zip_code_prefix LIKE '[0-9][0-9][0-9][0-9]%'),
     CONSTRAINT seller_state_format CHECK (seller_state LIKE '[A-Z][A-Z]')
 );
-
--- Product Category Name Translation
+    """
+#Product Category Name Translation
+product_category_name_translation_table_sql = """
 DROP TABLE IF EXISTS product_category_name_translation;
 CREATE TABLE product_category_name_translation (
     product_category_name VARCHAR(60) NOT NULL,
@@ -125,3 +134,17 @@ CREATE TABLE product_category_name_translation (
 
     CONSTRAINT PK__product_category_name_translation PRIMARY KEY (product_category_name)
 );
+  """
+
+
+TABLES = {
+    'customers': customers_table_sql,
+    'geolocation': geolocation_table_sql,
+    'order_items': orderItems_table_sql,
+    'order_payments': orderPayments_table_sql,
+    'order_reviews': orderReviews_table_sql,
+    'orders': orders_table_sql,
+    'products': products_table_sql,
+    'sellers': sellers_table_sql,
+    'product_category_name_translation': product_category_name_translation_table_sql
+}
